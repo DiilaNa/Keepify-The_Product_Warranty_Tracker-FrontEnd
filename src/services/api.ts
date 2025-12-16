9dbd98cff85946ca6a262d1a7a9df487c0e5dd69
DiilaNa <liyanaarachchidilan@gmail.com>
2025-12-16 14:17:12 +0530

    updated base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,7 @@ import { refreshTokens } from "./auth";
 const api = axios.create({
   baseURL:
     "https://keepify-the-product-warranty-tracke-theta.vercel.app/api/v1",
+  withCredentials: true,
 });
 
 const PUBLIC_ENDPOINTS = ["/auth/register", "/auth/login"];
